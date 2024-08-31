66929fde6d03698c3467d359afa15c6458eaf93f
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-08-31 12:28:08 +0200

    Creata Array + base in Html

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ const { createApp } = Vue
 
   createApp({
     data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
+        return {
+          todoList: [
+              { task: "Svegliarsi la mattina", done: true},
+              { task: "Aprire Zoom", done: true},
+              { task: "Non fare nulla", done: false},
+              { task: "Studiare", done: true},
+              { task: "Seguire lezione", done: true},
+              { task: "Dormire", done: false},
+              { task: "Fare domande", done: true}
+          ],
+          taskToAdd: ""
+        }
+  }
   }).mount('#app')
