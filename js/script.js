49f2b46e286f4d8c64be037c1d3454fd66460d7e
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-02 11:02:31 +0200

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -3,16 +3,39 @@ const { createApp } = Vue
   createApp({
     data() {
         return {
-          todoList: [
-              { task: "Svegliarsi la mattina", done: true},
-              { task: "Aprire Zoom", done: true},
-              { task: "Non fare nulla", done: false},
-              { task: "Studiare", done: true},
-              { task: "Seguire lezione", done: true},
-              { task: "Dormire", done: false},
-              { task: "Fare domande", done: true}
+          todos: [
+              { text: "Svegliarsi la mattina", 
+                done: true},
+              { text: "Aprire Zoom", 
+                done: true},
+              { text: "Non fare nulla", 
+                done: false},
+              { text: "Studiare", 
+                done: true},
+              { text: "Seguire lezione", 
+                done: true},
+              { text: "Dormire", 
+                done: false},
+              { text: "Fare domande", done: true}
           ],
-          taskToAdd: ""
-        }
+          newTodo: ''
+        };
+  },
+  methods: {
+    addTodo() {
+      const cleanTodo = this.newTodo.trim();
+
+      if (cleanTodo != '') {
+        this.todos.push({
+          text: cleanTodo,
+          done: false
+        });
+
+        this.newTodo = '';
+      }
+    },
+    removeTodo(i) {
+      this.todos.splice(i, 1);
+    }
   }
   }).mount('#app')
